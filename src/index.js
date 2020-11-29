d940af84aadd78f1f72dedd67de4617f13068b5b
Jimmy404 <bruno_pier@hotmail.com>
2020-11-29 16:19:19 -0300

    Adds component import into index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { CounterApp } from './component/useState/CounterApp';
+import { CounterCustomHook } from './component/useState/CounterCustomHook';
 
 ReactDOM.render(
-    <CounterApp />,
+    <CounterCustomHook />,
   document.getElementById('root')
 );
