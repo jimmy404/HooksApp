fdb4960e7ebbd868750ac8d5aa26d474920b9bbf
Jimmy404 <bruno_pier@hotmail.com>
2020-11-30 16:01:24 -0300

    Adds FocusScreen into index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { MultipleHooks } from './component/examples/MultipleHooks';
+import { FocusScreen } from './component/useRef/FocusScreen';
 
 ReactDOM.render(
-    <MultipleHooks />,
+    <FocusScreen />,
   document.getElementById('root')
 );
