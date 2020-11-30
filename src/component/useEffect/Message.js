b239ec9bc4ad8b4d2c167873468c2e8fae13f892
Jimmy404 <bruno_pier@hotmail.com>
2020-11-30 12:15:00 -0300

    Adds example to remove effect

--- src/component/useEffect/Message.js
+++ src/component/useEffect/Message.js
@@ -1,15 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const Message = () => {
+
+  const [coords, setCoords] = useState({ x:0, y:0 });
+  const { x, y } = coords;
+
   useEffect(() => {
-    console.log('Component mounted')
-    return () => {
-      console.log('Component dismounted')
+
+    const mouseMove = (e) => {
+      const coords = { x: e.x, y: e.y };
+      setCoords(coords);
+      console.log(coords);
     }
+
+    window.addEventListener('mousemove', mouseMove);
+
+    return () => {
+      console.log('Component dismounted');
+      window.removeEventListener('mousemove', mouseMove);
+    }
+
   }, [])
+
   return (
     <div>
       <h3>You rocks</h3>
+      <p>
+        x:{x} y:{y}
+      </p>
     </div>
   )
 }
