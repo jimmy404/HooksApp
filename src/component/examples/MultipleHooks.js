83a60d1dd1d2c340ceb95461f21c48effefdc95c
Jimmy404 <bruno_pier@hotmail.com>
2020-11-30 15:49:22 -0300

    Adds MultipleHooks code

--- src/component/examples/MultipleHooks.js
+++ src/component/examples/MultipleHooks.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import { useCounter } from '../../hooks/useCounter';
 import { useFetch } from '../../hooks/useFetch';
 import './effect.css';
 
 export const MultipleHooks = () => {
-  const state = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
-  console.log(state)
+  const { counter, increment } = useCounter(1);
+
+  const { loading, data } = useFetch(`https://www.breakingbadapi.com/api/quotes/${counter}`);
+  const { author, quote } = !!data && data[0];
+
   return (
     <div>
-      <h1>Custom Hook!</h1>
+      <h1>Breaking Bad</h1>
+      <hr/>
+      {
+        loading
+        ?
+          (
+            <div className="alert alert-info text-center">
+              Loading...
+            </div>
+          )
+        :
+          (
+            <blockquote className="blockquote text-right">
+              <p className="mb-0">{quote}</p>
+          <footer className="blockquote-footer">{author}</footer>
+            </blockquote>
+          )
+      }
+      <button className="btn btn-primary" onClick={increment}>Next quote</button>
     </div>
   )
 }
