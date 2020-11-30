2030c712bab7b0659acb2e9606edd5307e0f4fa3
Jimmy404 <bruno_pier@hotmail.com>
2020-11-30 12:44:39 -0300

    Adds FormCustomHook into index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { SimpleForm } from './component/useEffect/SimpleForm';
+import { FormCustomHook } from './component/useEffect/FormCustomHook';
 
 ReactDOM.render(
-    <SimpleForm />,
+    <FormCustomHook />,
   document.getElementById('root')
 );
