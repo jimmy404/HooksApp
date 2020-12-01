817ad2868113912c81ea5988a4c33403e12088ca
Jimmy404 <bruno_pier@hotmail.com>
2020-12-01 00:05:37 -0300

    Modify useFetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,7 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export const useFetch = ( url ) => {
+  const isMounted = useRef(true);
   const [state, setState] = useState({data: null, loading: true, error: null});
+
+  useEffect(() => {
+    return () => {
+      isMounted.current = false;
+    }
+  }, [])
 
   useEffect(() => {
 
@@ -11,11 +18,17 @@ export const useFetch = ( url ) => {
       .then(resp => resp.json())
       .then(data => {
 
-      setState({
-        loading: false,
-        error: null,
-        data
-      })
+        setTimeout(() => {
+          if (isMounted.current) {
+            setState({
+              loading: false,
+              error: null,
+              data
+            });
+          } else {
+            console.log('Not mounted');
+          }
+        }, 3000);
     });
   }, [url])
 
