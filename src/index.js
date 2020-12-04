107316552dd5aaf1ddbeaf2a343667da54c1336f
Jimmy404 <bruno_pier@hotmail.com>
2020-12-04 00:22:09 -0300

    Adds CallbackHook into index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Memo } from './component/memo/Memo';
+import { CallbackHook } from './component/memo/CallbackHook';
 
 ReactDOM.render(
-    <Memo />,
+    <CallbackHook />,
   document.getElementById('root')
 );
